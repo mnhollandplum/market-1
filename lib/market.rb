790b0921c7a1572da976506533a012d850fb4f23
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-09-12 14:37:48 -0600

    completes iteration_3

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Market
     attr_reader :name,
               :vendors
@@ -23,4 +24,29 @@ class Market
       end
     end.compact
   end
+
+  def sorted_item_list
+    get_inventory.map do |name, amount|
+      name.map do |name, amount|
+        name
+      end
+    end.flatten.uniq.sort
+  end
+
+
+  def total_inventory
+    get_inventory.inject(Hash.new(0)) do |item, total_inventory|
+      total_inventory.each do |total_amount, amount|    item[total_amount]
+      item[total_amount] += amount
+    end
+     item
+   end
+  end
+
+  def get_inventory
+    @vendors.map do |vendor|
+      vendor.inventory
+    end
+  end
+
 end
